47c567845c1dbd7e5c60686461570d3c596a94eb
GarthTB | 天卜 <g-art-h@outlook.com>
2025-12-29 20:58:15 +0800

    更新README

--- KmDictCleaner/Program.cs
+++ KmDictCleaner/Program.cs
@@ -2,16 +2,20 @@
 using static System.Console;
 
 try {
-    WriteLine("空明码词库清理工具 v1.0.0 (20251228)");
+    WriteLine("空明码词库清理工具 v1.0.0 (20251229)");
     WriteLine("作者：Garth TB <g-art-h@outlook.com>");
+
     WriteLine("开始清理...");
     var (pre, post) = DictFile.Proc(
-        @"G:\Code\Csharp\AAAA-TESTS\MasterDit.shp",
-        [static (code, word) => !FaultCheck.Char2(code, word, '冕', 's')]);
+        @"G:\Code\Csharp\AAAA-TESTS\MasterDit.shp", // 输入文件（词库）路径
+        [
+            static (code, word) => !FaultCheck.Char1(code, word, '冕', 'M'),
+            static (code, word) => !FaultCheck.Char2(code, word, '冕', 's')
+        ]);
+    WriteLine("清理完成。");
 
     var removed = pre - post;
     var ratio = 100d * removed / pre;
-    WriteLine("清理完成。");
     WriteLine($"清理前总词数：{pre}");
     WriteLine($"清理后总词数：{post}");
     WriteLine($"清理掉词数：{removed}");
